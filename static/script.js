d46ff87b9af8a789a8711546d712a83aae10cf8c
Iqmalbasori667 <iqmalbulusari@gmail.com>
2024-03-04 12:13:38 -0500

    Commit Kedua

--- static/script.js
+++ static/script.js
@@ -42,7 +42,7 @@ const sendMessage = async (message) => {
   messagesContainer.appendChild(loadingtextElement);
 
   async function makePostRequest(msg) {
-    const url = 'www.example.com';  // Make a POST request to this url
+    const url = 'https://efkan-5000.theia-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/chatbot';  // Make a POST request to this url
     const requestBody = {
       prompt: msg
     };
